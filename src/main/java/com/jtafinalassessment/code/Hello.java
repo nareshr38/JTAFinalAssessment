6548c26035c636eaf186fe8f3b650f6c6e28b063
Naresh <naresh_rangu@epam.com>
2019-11-07 17:58:31 +0530

    commit testpgm and log4j

--- src/main/java/com/jtafinalassessment/code/Hello.java
+++ src/main/java/com/jtafinalassessment/code/Hello.java
@@ -1,7 +1,12 @@
 package com.jtafinalassessment.code;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class Hello {
+    private static final Logger LOGGER = LogManager.getLogger(Hello.class.getName());
     public static void main(String[] args) {
-        System.out.println("Naresh");
+        LOGGER.info("Rangu");
     }
 }
